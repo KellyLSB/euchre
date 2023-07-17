37a1a19a0f6e602e4ed5ba94ac272a216099cc2e
☆ Kelly Lauren Summer Becker–Neuding ♡ <kbecker@kellybecker.me>
2023-07-16 21:41:49 -0700

    Adding Euchre base code. Trying to implement the UI.

--- app/src/main/java/me/kellybecker/android/euchre/MainActivity.kt
+++ app/src/main/java/me/kellybecker/android/euchre/MainActivity.kt
@@ -1,28 +1,121 @@
 package me.kellybecker.android.euchre
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.kellybecker.android.euchre.logic.Game
 import me.kellybecker.android.euchre.ui.theme.EuchreTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            EuchreTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        setContent { MainActivityContent() }
+    }
+}
+
+@Composable
+fun MainActivityContent() {
+    EuchreTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            val gameInstance = Game()
+
+            // Assuming local hosted game
+            gameInstance.shuffle()
+            gameInstance.cut(/* onMyCut = { /*  */ } */)
+            gameInstance.deal()
+
+            Log.d("CARDS", gameInstance.hands.toString())
+
+            CardTable(0, gameInstance)
+        }
+    }
+}
+
+@Composable
+fun CardTable(player: Int, gameInstance: Game) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        // Player 2
+        Row(modifier = Modifier.rotate(180.0F), horizontalArrangement = Arrangement.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Greeting("Player ${player + 2 % 4}, Cards: ${gameInstance.hands[player + 2 % 4].size}")
+                Row {
+                    gameInstance.hands[player + 2 % 4].forEach {
+                        GameCard(it.suit, it.card)
+                    }
+                }
+            }
+        }
+        Row(modifier = Modifier
+            .height(235.dp)
+            .wrapContentSize(unbounded = true)) {
+            // Player 1
+            Row(modifier = Modifier.rotate(90.0F)) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Greeting("Player ${player + 1 % 4}, Cards: ${gameInstance.hands[player + 1 % 4].size}")
+                    Row {
+                        gameInstance.hands[player + 1 % 4].forEach {
+                            GameCard(it.suit, it.card)
+                        }
+                    }
+                }
+            }
+            Column(modifier = Modifier.height(45.dp).wrapContentSize(unbounded = true)) {
+                gameInstance.kitty.forEach {
+                    GameCard(it.suit, it.card)
+                }
+            }
+            Column(modifier = Modifier.height(45.dp).wrapContentSize(unbounded = true)) {
+                gameInstance.kitty.forEach {
+                    GameCard(it.suit, it.card)
+                }
+            }
+            // Player 3
+            Row(modifier = Modifier.rotate(-90.0F)) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Greeting("Player ${player + 3 % 4}, Cards: ${gameInstance.hands[player + 3 % 4].size}")
+                    Row {
+                        gameInstance.hands[player + 3 % 4].forEach {
+                            GameCard(it.suit, it.card)
+                        }
+                    }
+                }
+            }
+        }
+        // Player 0
+        Row(horizontalArrangement = Arrangement.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Greeting("Player ${player + 0 % 4}, Cards: ${gameInstance.hands[player + 0 % 4].size}")
+                Row {
+                    gameInstance.hands[player + 0 % 4].forEach {
+                        GameCard(it.suit, it.card)
+                    }
                 }
             }
         }
@@ -37,10 +130,26 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun GameCard(suit: String, face: String) {
+    val red = listOf("♥", "♦")
+
+    val suitColor = if(suit in red) {
+        Color.Red
+    } else {
+        Color.Black
+    }
+
+    Card(modifier = Modifier.padding(2.5.dp)) {
+        Row(modifier = Modifier.padding(1.7.dp, end = 3.2563.dp)) {
+            Text(suit, color = suitColor, fontWeight = FontWeight.Black)
+            Text(face)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    EuchreTheme {
-        Greeting("Android")
-    }
+fun MainActivityPreview() {
+    MainActivityContent()
 }
---
+++ app/src/main/java/me/kellybecker/android/euchre/logic/Euchere.kt
@@ -0,0 +1,482 @@
+package me.kellybecker.android.euchre.logic
+
+var trump: String = ""
+
+/**
+ * Check if suit is a bower suit
+ */
+fun isBowerSuit(suit: String): Boolean {
+    val black = listOf("♠", "♣")
+    val red = listOf("♥", "♦")
+    return (suit in black && trump in black) 
+        || (suit in red && trump in red)
+}
+
+/**
+ * Check if suit qualifies as a bower
+ */
+fun isBower(suit: String): String {
+    if(isBowerSuit(suit)) {
+        return if(suit == trump) { "J1" } else { "J2" }
+    } else {
+        return "J"
+    }
+}
+
+val scoreOrder: List<String> = listOf(
+    "T", "J1", "J2", "J", "A", "K", "Q", "10", "9",
+    "AJ", "AK", "AQ", "A10", "A9",
+)
+
+/**
+ * Weights for ordering the cards by scoring values
+ */
+fun scoreOrderIndex(card: Card, reverse: Boolean = false): Int {
+    val tmpScoreOrder: List<String> = if(reverse) {
+        scoreOrder.asReversed()
+    } else {
+        scoreOrder.toList()
+    }
+    
+    return if(trump != "" && card.card == "J" && isBowerSuit(card.suit)) {
+        //println("\t${card.suit}${isBower(card.suit)}")
+        tmpScoreOrder.indexOf(isBower(card.suit))
+    } else {
+        if(trump != "" && card.card != "T" && trump != card.suit) {
+            //println("\t${card.suit}A${card.card}")
+            tmpScoreOrder.indexOf("A${card.card}")
+        } else {
+            //println("\t${card.suit}${card.card}")
+            tmpScoreOrder.indexOf(card.card)
+        }
+    }
+}
+
+/**
+ * All the hands in the game, deck and kitty
+ */
+class Game {
+    // Keep track of cards
+    val deck: Deck = Deck()
+    val hands: List<Hand> = listOf<Hand>(
+        Hand(0), Hand(1), Hand(2), Hand(3),
+    )
+    val kitty: Stack = Stack()
+
+    // Track the turn and dealer
+    var turn: Int = 0
+    var dealer: Int = 0
+    var goingAlone: Int = -1
+
+    // Trick currently in play
+    var trick: Int = 0
+    var trickCards: Trick? = null
+
+    // Shuffle the cards
+    fun shuffle() { deck.shuffle() }
+    // Cut the deck
+    fun cut() { deck.cut() }
+    // What is trump
+    fun trump(): String { return trump }
+
+    /**
+     * Deal out the cards
+     */
+    fun deal() {
+        var hand = dealer
+        var cards = 2
+        while(deck.size > 0) {
+            if(hands[hand].size < 5) {
+                for(i in 0..cards) {
+                    if(hands[hand].size < 5) {
+                        hands[hand].add(deck.removeFirst())
+                    }
+                }
+
+                if(hand < 3) {
+                    hand++
+                    when(cards) {
+                        1 -> cards = 2
+                        2 -> cards = 1
+                    }
+                } else {
+                    hand = 0
+                }
+            } else {
+                while(deck.size > 0) {
+                    kitty.add(deck.removeFirst())
+                }
+            }
+        }
+    }
+
+    fun play(
+        onShouldPickItUp: ((Hand, Stack) -> Boolean)? = null,
+        onSelectTrump: ((Hand) -> String)? = null,
+        onShouldGoAlone: ((Hand) -> Boolean)? = null,
+        onPlay: ((Hand, Trick) -> Unit)? = null,
+    ) {
+        // Select trump by dealer/kitty exchange
+        while(turn < 4) {
+            if(hands[whoseTurn()].shouldPickItUp(kitty)) {
+                if(kitty[0].suit != "T") {
+                    trump = kitty[0].suit
+                    println("${whoseTurn()} told the dealer to pick up Kitty card\nTrump Selected by Kitty: ${trump}")
+                }
+                hands[dealer].pickItUp(kitty)
+                break
+            }
+
+            turn++
+        }
+        turn = 0
+        
+        // Select trump by best hand
+        while(trump == "" && turn < 4) {
+            trump = hands[whoseTurn()].selectTrump()
+            if(turn == 3 && trump == "") {
+                return // Skip hand
+            }
+
+            if(trump != "") {
+                println("Trump Selected by ${whoseTurn()}: ${trump}")
+                break
+            }
+            
+            turn++
+        }
+        turn = 0
+
+        // Go alone for a round
+        while(turn < 4) {
+            if(hands[whoseTurn()].shouldGoAlone()) {
+                goingAlone = whoseTurn()
+                break
+            }
+            turn++
+        }
+        turn = 0
+
+        println(this)
+
+        while(trick < 4) {
+            trickCards = Trick()
+            while(turn < 4) {
+                // Skip player if partner is going alone.
+                if(goingAlone > -1 && goingAlone != whoseTurn()) {
+                    if(goingAlone % 2 == whoseTurn() % 2) {
+                        println("${goingAlone} is going alone.")
+                        turn++
+                        continue
+                    }
+                }
+
+                hands[whoseTurn()].play(trickCards)
+                println("\t${trickCards}")
+                turn++
+            }
+            turn = 0
+            
+            hands[trick.winningHand()].tricks.add(trickCards)
+            trick++
+        }
+    }
+
+    fun nextHand() {
+        if(dealer < 3) {
+            dealer++
+        } else {
+            dealer = 0
+        }
+
+        turn = 0
+        trump = ""
+        goingAlone = -1
+
+        deck.reset()
+        for(hand in hands) {
+            hand.reset()
+        }
+        kitty.reset()
+    }
+
+    fun whoseTurn(): Int {
+        return (dealer + turn) % 4
+    }
+
+    override fun toString(): String {
+        return "Trump: ${trump}\nDeck: ${deck}\nHands: ${hands}\nKitty: ${kitty}\n"
+    }
+}
+
+/**
+ * Card object
+ */
+class Card(suit: String, card: String) {
+    val suit: String = suit
+    val card: String = card
+
+    // Suitless comparision
+    operator fun compareTo(b: Card): Int {
+        println("\t${card}: ${scoreOrder.indexOf(card)}")
+        println("\t${b.card}: ${scoreOrder.indexOf(b.card)}")
+        var cardA = card
+        var cardB = b.card
+
+        if(cardA == "J") {
+            cardA = isBower(suit)
+        }
+
+        if(cardB == "J") {
+            cardB = isBower(b.suit)
+        }
+
+        val tmp = scoreOrder.indexOf(cardA).compareTo(
+            scoreOrder.indexOf(cardB)
+        )
+
+        return when(tmp) {
+            -1 -> 1
+            1  -> -1
+            else -> 0
+        }
+    }
+
+    override fun toString(): String {
+        return "${this.suit}${this.card}"
+    }
+}
+
+class Trick : MutableMap<Int, Card> by mutableMapOf() {
+    var suit: String = ""
+
+    fun winningHand(): Int {
+        println("\tBestPlay: ${bestPlay()}")
+        return bestPlay().first
+    }
+
+    fun bestCard(): Card {
+        return bestPlay().second
+    }
+
+    fun bestPlay(): Pair<Int, Card> {
+        return toList().sortedBy { (_, card) ->
+            scoreOrderIndex(card)
+        }[0]
+    }
+
+    fun play(hand: Int, card: Card): Card? {
+        if(suit == "" && size < 1) {
+            suit = card.suit
+            if(suit == "T") {
+                suit = trump
+            }
+        }
+
+        return put(hand, card)
+    }
+
+    override fun toString(): String {
+        return toMap().toString()
+    }
+}
+
+/**
+ * A stack of cards
+ */
+open class Stack : MutableList<Card> by mutableListOf() {
+
+    /**
+     * Cut the stack of cards by a random amount
+     */
+    fun cut() {
+        val middle = size / 2
+        val cutPnt = ((middle-3)..(middle+2)).random()
+        val newList = listOf(
+            subList(0, cutPnt), subList(cutPnt, size),
+        ).reversed().flatten()
+
+        removeAll{true}
+        addAll(newList)
+    }
+
+    // Empty the stack
+    open fun reset() { removeAll{true} }
+
+    // Suitable cards
+    open fun suited(suit: String): Stack {
+        val tmp = Stack()
+        tmp.addAll(filter { 
+            if(it.card == "J" && isBower(it.suit) == "J2") {
+                if(trump == suit) {
+                    println("\tLeft bower suited")
+                }
+                trump == suit
+            } else {
+                it.suit == suit
+            }
+        })
+        return tmp
+    }
+
+    // Best cards
+    fun bestCards(): Stack {
+        val tmp = Stack()
+        tmp.addAll(this.sortedBy {
+            scoreOrderIndex(it)
+        })
+        return tmp
+    }
+
+    // Throw away
+    fun throwCards(): Stack {
+        val tmp = Stack()
+        tmp.addAll(this.sortedBy {
+            scoreOrderIndex(it, true)
+        })
+        return tmp
+    }
+
+    override fun toString(): String {
+        return toMutableList().toString()
+    }
+}
+
+class Hand(hand: Int) : Stack() {
+    val tricks: MutableList<Trick> = mutableListOf()
+    val hand: Int = hand
+
+    fun pickItUp(kitty: Stack) {
+        //@TODO query discard input
+        val disCard = (0..4).random()
+        kitty.add(removeAt(disCard))
+        add(kitty.removeFirst())
+    }
+
+    fun shouldPickItUp(kitty: Stack): Boolean {
+        //@TODO query user input
+        val avgScore = calculateIdealTrump()
+        return avgScore.size > 0 && avgScore[0].first == kitty[0].suit
+    }
+
+    fun shouldGoAlone(): Boolean {
+        //@TODO query user input
+        //@TODO write ai for perfect play
+        return calculateGoAlone().size > 0
+    }
+
+    /**
+     * Calculate the average scoring range of cards per suit.
+     */
+    fun calculate(vararg suits: String): MutableMap<String, Pair<Int, Int>> {
+        val suits = if(suits.size > 0) { suits } else { arrayOf("♠", "♥", "♣", "♦") }
+        val avgScores: MutableMap<String, Pair<Int, Int>> = mutableMapOf()
+        for(suit in suits) {
+            var suitAvgScore: MutableList<Int> = mutableListOf()
+            for(card in suited(suit).bestCards()) {
+                suitAvgScore.add(scoreOrderIndex(card))
+            }
+            avgScores.put(suit, Pair(suitAvgScore.size, suitAvgScore.average().toInt()))
+        }
+
+        return avgScores
+    }
+
+    fun calculateGoAlone(): List<Pair<String, Pair<Int, Int>>> {
+        return calculate(trump).filterValues { it.first > 2 }  // 3 or more cards in a suit
+                               .filterValues { it.second < 7 } // Scoring 6 or less
+                               .filterValues { it.second > 2 } // Scoring 3 or more
+                               .toList().sortedBy { (_, v) -> v.first }
+    }
+
+    /**
+     * Calculate the ideal trump based on average scoring cards per suit.
+     */
+    fun calculateIdealTrump(): List<Pair<String, Pair<Int, Int>>> {
+        return calculate().filterValues { it.first > 1 }  // 2 or more cards in a suit
+                          .filterValues { it.second < 6 } // Scoring 5 or less
+                          .filterValues { it.second > 2 } // Scoring 3 or more
+                          .toList().sortedBy { (_, v) -> v.first }
+    }
+
+    fun selectTrump(): String {
+        //@TODO query user input
+        val avgScore = calculateIdealTrump()
+
+        // The first is the ideal
+        if (avgScore.size > 0) {
+            return avgScore[0].first
+        }
+
+        return ""
+    }
+
+    fun play(trick: Trick) {
+        //return @TODO query user input OR
+        //@TODO chance of randomness maybe
+        // a human might save a higher scoring card
+        if(trick.size > 0) {
+            val bestCard = trick.bestCard()
+            val ourCards = suited(trick.suit).bestCards()
+            if(ourCards.size > 0) {
+                if(bestCard > ourCards[0]) {
+                    println("Their card is better")
+                    remove(ourCards.last())
+                    trick.play(this.hand, ourCards.last())
+                } else {
+                    println("Our cards are better")
+                    remove(ourCards.first())
+                    trick.play(this.hand, ourCards.first())
+                }
+            } else {
+                println("Can't follow suit")
+                val ourCards = throwCards()
+                remove(ourCards.first())
+                trick.play(this.hand, ourCards.first())
+            }
+        } else {
+            val perfectGame = this.sortedBy {
+                scoreOrderIndex(it)
+            }
+
+            remove(perfectGame[0])
+            trick.play(this.hand, perfectGame[0])
+        }
+    }
+}
+
+/**
+ * Initialize a deck of cards
+ */
+class Deck : Stack() {
+    init { reset() }
+    override fun reset() {
+        super.reset()
+
+        for (suit in listOf("♠", "♥", "♣", "♦")) {
+            for(card in listOf("A", "K", "Q", "J", "10", "9")) {
+                add(Card(suit, card))
+            }
+        }
+
+        add(Card("T", "T"))
+    }
+}
+
+fun main() {
+    val game = Game()
+    for(i in (0..0)) {
+        println("Game ${i}")
+        game.shuffle()
+        game.cut()
+        game.deal()
+        println(game)
+        game.play()
+        println(game)
+        game.nextHand()
+    }
+
+    for(hand in game.hands) {
+        println("${hand.hand}: ${hand.tricks}")
+    }
+}
